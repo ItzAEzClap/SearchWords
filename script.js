7fe1d18111ed6eef1a9396757c6aa3e51381eb0b
ItzAEzClap <karlsson_@outlook.com>
2023-04-16 00:10:20 +0200

    FUCK

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ async function main() {
         let word = words[Math.floor(Math.random() * words.length)]
         let newWindow = window.open(`http://www.bing.com/search?q=` + encodeURIComponent(word))
         await new Promise(resolve => setTimeout(resolve, delay))
+    }
 }
 
 amount.addEventListener('input', () => amount.value = amount.value > 100 ? 100 : amount.value)
